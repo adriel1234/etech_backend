db17cb4424b191c85bf93f215ddffa263617c255
Adriel Vinicius <37392070+adriel1234@users.noreply.github.com>
2024-10-11 18:20:20 -0400

    update settings.py paginations

--- tecommerce/tecommerce/settings.py
+++ tecommerce/tecommerce/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'teste.apps.TesteConfig',
     'rest_framework',
     'django_filters',
@@ -139,4 +140,6 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
