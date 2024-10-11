9e45af8c0a9224e1a3bb88dd6da7cb61043d561f
Adriel Vinicius <37392070+adriel1234@users.noreply.github.com>
2024-10-11 19:27:36 -0400

    alteração serializers

--- tecommerce/teste/serializers.py
+++ tecommerce/teste/serializers.py
@@ -1,24 +1,27 @@
 from rest_framework import serializers
 from teste import models
 
+
 class ClientSerializer(serializers.ModelSerializer):
+    age = serializers.IntegerField(min_value=18)
     class Meta:
         model = models.Client
         exclude = ['created_at']
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Product
         exclude = ['created_at']
 
+
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Employee
         exclude = ['created_at']
 
+
 class SaleSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Sale
         exclude = ['created_at']
-
-
