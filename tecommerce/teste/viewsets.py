a14ad0d32ed0f04137ff4adcbb2c8a9d47e76bda
Adriel Vinicius <37392070+adriel1234@users.noreply.github.com>
2024-10-11 21:18:37 -0400

    add endpoint teste

--- tecommerce/teste/viewsets.py
+++ tecommerce/teste/viewsets.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from teste import models, serializers, filters
 
 
@@ -8,12 +11,25 @@ class ClientViewSet(viewsets.ModelViewSet):
     filterset_class = filters.ClientFilter
     permission_classes = {permissions.IsAuthenticated}
 
+    @action(detail=False, methods=['GET'])
+    def teste(self, request, *args, **kwargs):
+        return Response({'teste': 1}, status=200)
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = models.Product.objects.all()
     serializer_class = serializers.ProductSerializer
     filterset_class = filters.ProductFilter
     permission_classes = {permissions.IsAuthenticated}
+
+    @action(detail=False, methods=['GET'])
+    def product_teste(self, request, *args, **kwargs):
+        # products = self.queryset.all().values('description', 'quantity')
+        # products = self.queryset.all()
+        products = self.queryset.get(id=1)
+        p_serializer = self.get_serializer(products)
+
+        return Response(p_serializer.data, status=200)
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
